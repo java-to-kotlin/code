20229cf0088449b1e63632a4dc4bed83716f6238
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-07-19 18:48:02 +0100

    errors.19 : Simplify handle (fails tests)

--- src/main/java/travelator/handlers/CustomerRegistrationHandler.kt
+++ src/main/java/travelator/handlers/CustomerRegistrationHandler.kt
@@ -2,9 +2,12 @@ package travelator.handlers
 
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
-import dev.forkhandles.result4k.Failure
-import dev.forkhandles.result4k.Success
-import travelator.*
+import dev.forkhandles.result4k.map
+import dev.forkhandles.result4k.recover
+import travelator.Duplicate
+import travelator.Excluded
+import travelator.IRegisterCustomers
+import travelator.RegistrationProblem
 import travelator.http.Request
 import travelator.http.Response
 import java.net.HttpURLConnection.*
@@ -14,31 +17,25 @@ class CustomerRegistrationHandler(
 ) {
     private val objectMapper = ObjectMapper()
 
-    fun handle(request: Request): Response {
-        return try {
+    fun handle(request: Request): Response =
+        try {
             val data = objectMapper.readValue(
                 request.body,
                 RegistrationData::class.java
             )
-            val customerResult = registration.registerToo(data)
-            when (customerResult) {
-                is Success -> Response(
-                    HTTP_CREATED,
-                    objectMapper.writeValueAsString(customerResult.value)
-                )
-                is Failure -> customerResult.reason.toResponse()
-
-            }
+            registration.registerToo(data)
+                .map { value ->
+                    Response(
+                        HTTP_CREATED,
+                        objectMapper.writeValueAsString(value)
+                    )
+                }
+                .recover { reason -> reason.toResponse() }
         } catch (x: JsonProcessingException) {
             Response(HTTP_BAD_REQUEST)
-        } catch (x: ExcludedException) {
-            Response(HTTP_FORBIDDEN)
-        } catch (x: DuplicateException) {
-            Response(HTTP_CONFLICT)
         } catch (x: Exception) {
             Response(HTTP_INTERNAL_ERROR)
         }
-    }
 }
 
 private fun RegistrationProblem.toResponse() = when (this) {
