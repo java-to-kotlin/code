87d1f831ded8371ede6db55f94174a74ef363323
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-07-19 14:25:22 +0100

    errors.13 : Implement addToo in InMemoryCustomers

--- src/test/java/travelator/InMemoryCustomers.java
+++ src/test/java/travelator/InMemoryCustomers.java
@@ -1,5 +1,9 @@
 package travelator;
 
+import dev.forkhandles.result4k.Failure;
+import dev.forkhandles.result4k.Result;
+import dev.forkhandles.result4k.Success;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +23,23 @@ public class InMemoryCustomers implements Customers {
         Customer result = new Customer(Integer.toString(newId), name, email);
         list.add(result);
         return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Result<Customer, DuplicateException> addToo(
+        String name, String email
+    ) {
+        if (list.stream().anyMatch( item -> item.getEmail().equals(email)))
+            return new Failure<>(
+                new DuplicateException(
+                    "customer with email " + email + " already exists"
+                )
+            );
+        int newId = id++;
+        Customer result = new Customer(Integer.toString(newId), name, email);
+        list.add(result);
+        return new Success<Customer>(result);
     }
 
     @Override
