268d04ef284e4fcd617911d54cb9b763857d8edf
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-07-19 18:33:40 +0100

    errors.16 : Add registerToo

--- src/main/java/travelator/IRegisterCustomers.kt
+++ src/main/java/travelator/IRegisterCustomers.kt
@@ -1,9 +1,30 @@
 package travelator
 
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Success
 import travelator.handlers.RegistrationData
 
 interface IRegisterCustomers {
 
     @Throws(ExcludedException::class, DuplicateException::class)
     fun register(data: RegistrationData): Customer
+
+    fun registerToo(data: RegistrationData):
+        Result<Customer, RegistrationProblem> =
+        try {
+            Success(register(data))
+        } catch (x: ExcludedException) {
+            Failure(Excluded)
+        } catch (x: DuplicateException) {
+            Failure(Duplicate(x.message))
+        }
 }
+
+sealed class RegistrationProblem
+
+object Excluded : RegistrationProblem()
+
+data class Duplicate(
+    val message: String?
+) : RegistrationProblem()
