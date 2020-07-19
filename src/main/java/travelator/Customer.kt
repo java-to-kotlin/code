5dc3365c37e3a141079e412fc409cf65371b5984
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-07-19 11:57:44 +0100

    errors.1 : Convert Customer to Kotlin

--- src/main/java/travelator/Customer.kt
+++ src/main/java/travelator/Customer.kt
@@ -1,42 +1,7 @@
-package travelator;
+package travelator
 
-import java.util.Objects;
-
-public class Customer {
-    private final String id;
-    private final String name;
-    private final String email;
-
-    public Customer(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return id.equals(customer.id) &&
-            name.equals(customer.name) &&
-            email.equals(customer.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email);
-    }
-}
+data class Customer(
+    val id: String,
+    val name: String,
+    val email: String
+)
