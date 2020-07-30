3099df0499546638690638e6efddf5100011e4c5
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-07-30 15:11:22 +0100

    nullability.10 : Move longestLegOver to top level

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -2,22 +2,19 @@ package travelator
 
 import java.time.Duration
 
-object Legs {
-    fun longestLegOver(
-        legs: List<Leg>,
-        duration: Duration
-    ): Leg? {
-        var result: Leg? = null
-        for (leg in legs) {
-            if (isLongerThan(leg, duration))
-                if (result == null ||
-                    isLongerThan(leg, result.plannedDuration))
-                    result = leg
-        }
-        return result
+fun longestLegOver(
+    legs: List<Leg>,
+    duration: Duration
+): Leg? {
+    var result: Leg? = null
+    for (leg in legs) {
+        if (isLongerThan(leg, duration))
+            if (result == null ||
+                isLongerThan(leg, result.plannedDuration))
+                result = leg
     }
-
-    private fun isLongerThan(leg: Leg, duration: Duration): Boolean {
-        return leg.plannedDuration.compareTo(duration) > 0
-    }
+    return result
 }
+
+private fun isLongerThan(leg: Leg, duration: Duration) =
+    leg.plannedDuration.compareTo(duration) > 0
--- src/test/java/travelator/LongestLegOverTests.kt
+++ src/test/java/travelator/LongestLegOverTests.kt
@@ -3,7 +3,6 @@ package travelator
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
-import travelator.Legs.longestLegOver
 import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
