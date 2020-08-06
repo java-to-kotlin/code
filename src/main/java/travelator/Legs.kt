70ed665450d8b6bfb966fa3f40edc2c10f13ce5d
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-08-06 11:11:45 +0100

    nullability.16 : takeIf

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -6,9 +6,6 @@ fun longestLegOver(
     legs: List<Leg>,
     duration: Duration
 ): Leg? =
-    legs.maxByOrNull(Leg::plannedDuration)?.let { longestLeg ->
-        if (longestLeg.plannedDuration > duration)
-            longestLeg
-        else
-            null
+    legs.maxByOrNull(Leg::plannedDuration)?.takeIf { longestLeg ->
+        longestLeg.plannedDuration > duration
     }
