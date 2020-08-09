b595a0a7ff40ae7ffa0fc509d8ece69d715feea8
Nat Pryce <sw@natpryce.com>
2020-08-09 14:50:11 +0100

    values.4 : money baseline (marked up)

--- src/main/java/travelator/money/Money.java
+++ src/main/java/travelator/money/Money.java
@@ -2,11 +2,7 @@ package travelator.money;
 
 import java.math.BigDecimal;
 import java.util.Currency;
-import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import static java.math.BigDecimal.ZERO;
 
@@ -19,42 +15,36 @@ public class Money {
         this.currency = currency;
     }
 
+    public static Money of(BigDecimal amount, Currency currency) { // <1>
+        return new Money(
+            amount.setScale(currency.getDefaultFractionDigits()),
+            currency);
+    }
+
+
     public static Money of(String amountStr, Currency currency) { // <2>
         return Money.of(new BigDecimal(amountStr), currency);
     }
 
-    public static Money of(int amount, Currency currency) { // <2>
+    public static Money of(int amount, Currency currency) {
         return Money.of(new BigDecimal(amount), currency);
-    }
-
-    public static Money of(BigDecimal amount, Currency currency) {
-        return new Money(
-            amount.setScale(currency.getDefaultFractionDigits()),
-            currency);
     }
 
     public static Money zero(Currency userCurrency) {
         return Money.of(ZERO, userCurrency);
     }
 
-    public BigDecimal getAmount() {
+
+    public BigDecimal getAmount() { // <2>
         return amount;
     }
 
-    public Currency getCurrency() {
+    public Currency getCurrency() { // <3>
         return currency;
     }
 
-    public Money add(Money that) {
-        if (!this.currency.equals(that.currency)) {
-            throw new IllegalArgumentException("cannot add Money values of different currencies");
-        }
-
-        return Money.of(this.amount.add(that.amount), this.currency);
-    }
-
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) { // <3>
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Money money = (Money) o;
@@ -63,19 +53,21 @@ public class Money {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() { // <3>
         return Objects.hash(amount, currency);
     }
 
     @Override
-    public String toString() {
+    public String toString() { // <4>
         return amount.toString() + " " + currency.getCurrencyCode();
     }
 
-    public static Collector<Money, ?, Map<Currency, Money>> sumByCurrency() {
-        return Collectors.toMap(
-            Money::getCurrency,
-            Function.identity(),
-            Money::add);
+    public Money add(Money that) { // <5>
+        if (!this.currency.equals(that.currency)) {
+            throw new IllegalArgumentException(
+                "cannot add Money values of different currencies");
+        }
+
+        return new Money(this.amount.add(that.amount), this.currency);
     }
 }
--- src/test/java/travelator/money/MoneyTest.java
+++ src/test/java/travelator/money/MoneyTest.java
@@ -2,10 +2,8 @@ package travelator.money;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static travelator.money.Currencies.*;
 
 public class MoneyTest {
@@ -17,20 +15,13 @@ public class MoneyTest {
         assertEquals("1200 JPY", Money.of("1200", JPY).toString());
     }
 
-    @Test public void summing() {
-        final var moneys = List.of(
-            Money.of("100.00", GBP),
-            Money.of("200.00", EUR),
-            Money.of("25.00", GBP),
-            Money.of("1500.000", JOD),
-            Money.of("300.00", EUR));
+    @Test public void adding() {
+        assertEquals(Money.of(275, EUR), Money.of(200, EUR).add(Money.of(75, EUR)));
+        assertEquals(Money.of(266, GBP), Money.of(266, GBP).add(Money.zero(GBP)));
+    }
 
-        var amountsByCurrency = moneys.stream().collect(Money.sumByCurrency());
-
-        assertEquals(Map.of(
-            GBP, Money.of("125.00", GBP),
-            EUR, Money.of("500.00", EUR),
-            JOD, Money.of("1500.000", JOD)
-        ), amountsByCurrency);
+    @Test
+    public void cannot_add_money_of_different_currencies() {
+        assertThrows(IllegalArgumentException.class, () -> Money.of(1, GBP).add(Money.of(2, EUR)));
     }
 }
