ba6d587e3e12cfe8cc1d5b3a6f2e81ad5214cb56
Nat Pryce <sw@natpryce.com>
2020-08-16 19:59:08 +0100

    values.1 : mark up code replaced by primary constructor

--- src/main/java/travelator/money/CurrencyConversion.java
+++ src/main/java/travelator/money/CurrencyConversion.java
@@ -3,25 +3,24 @@ package travelator.money;
 import java.util.Objects;
 
 public class CurrencyConversion {
-    private final Money fromMoney; // <1>
+    private final Money fromMoney;
     private final Money toMoney;
 
-    // <2>
     public CurrencyConversion(Money fromMoney, Money toMoney) {
         this.fromMoney = fromMoney;
         this.toMoney = toMoney;
     }
 
-    public Money getFromMoney() { // <3>
+    public Money getFromMoney() {
         return fromMoney;
     }
 
-    public Money getToMoney() { // <3>
+    public Money getToMoney() {
         return toMoney;
     }
 
     @Override
-    public boolean equals(Object o) { // <4>
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CurrencyConversion that = (CurrencyConversion) o;
@@ -30,12 +29,12 @@ public class CurrencyConversion {
     }
 
     @Override
-    public int hashCode() { // <4>
+    public int hashCode() {
         return Objects.hash(fromMoney, toMoney);
     }
 
     @Override
-    public String toString() { // <5>
+    public String toString() {
         return "CurrencyConversion{" +
             "fromMoney=" + fromMoney +
             ", toMoney=" + toMoney +
