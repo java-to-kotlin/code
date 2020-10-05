e7aab3c85068cc2fdac10a66db99b3af2e4c0712
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-10-05 20:27:32 +0100

    beans-to-values.6 : Make UserPreferences immutable

--- src/main/java/travelator/mobile/UserPreferences.kt
+++ src/main/java/travelator/mobile/UserPreferences.kt
@@ -2,14 +2,8 @@ package travelator.mobile
 
 import java.util.*
 
-class UserPreferences(
-    var greeting: String,
-    var locale: Locale,
-    var currency: Currency
-) {
-    constructor() : this(
-        greeting = "Hello",
-        locale = Locale.UK,
-        currency = Currency.getInstance(Locale.UK)
-    )
-}
+data class UserPreferences(
+    val greeting: String,
+    val locale: Locale,
+    val currency: Currency
+)
