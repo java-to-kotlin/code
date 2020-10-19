6a124c05c621b607285cad8a812368bd033a1775
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-10-19 13:29:37 +0100

    Use 1.2 collections

--- src/main/java/travelator/Route.java
+++ src/main/java/travelator/Route.java
@@ -1,16 +1,17 @@
 package travelator;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Route {
-    private final Vector journeys = new Vector();
+    private final List<Journey> journeys = new ArrayList<>(); // <1>
 
     public int size() {
         return journeys.size();
     }
 
-    public Journey getJourneys(int index) {
-        return (Journey) journeys.elementAt(index);
+    public Journey getJourney(int index) {
+        return journeys.get(index); // <2>
     }
 
     public void addJourney(Journey journey) {
@@ -18,6 +19,6 @@ public class Route {
     }
 
     public Location getDepartsFrom() {
-        return getJourneys(0).getDepartsFrom();
+        return getJourney(0).getDepartsFrom();
     }
 }
