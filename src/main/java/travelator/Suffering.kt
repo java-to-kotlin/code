a753fb846545f8cfa73e534cacaf5f4caa4e9cfa
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-10 20:26:55 +0000

    collections.11 : Refactor bearable

--- src/main/java/travelator/Suffering.kt
+++ src/main/java/travelator/Suffering.kt
@@ -1,35 +1,28 @@
 package travelator
 
 import travelator.Other.SOME_COMPLICATED_RESULT
-import java.util.stream.Collectors
+import travelator.Other.routesFor
 
 object Suffering {
     @JvmStatic
-    fun sufferScoreFor(route: List<Journey>): Int {
-        return sufferScore(
-            route.longestJourneys(limit = 3), // <1>
+    fun sufferScoreFor(route: List<Journey>): Int =
+        sufferScore(
+            route.longestJourneys(limit = 3),
             Routes.getDepartsFrom(route)
         )
-    }
 
     @JvmStatic
     fun List<Journey>.longestJourneys(limit: Int): List<Journey> =
         sortedByDescending { it.duration }.take(limit)
 
-    fun routesToShowFor(itineraryId: String?): List<List<Journey>> {
-        return bearable(Other.routesFor(itineraryId))
-    }
+    fun routesToShowFor(itineraryId: String?): List<List<Journey>> =
+        bearable(routesFor(itineraryId))
 
-    private fun bearable(routes: List<List<Journey>>): List<List<Journey>> {
-        return routes.stream()
-            .filter { route -> sufferScoreFor(route) <= 10 }
-            .collect(Collectors.toUnmodifiableList())
-    }
+    private fun bearable(routes: List<List<Journey>>): List<List<Journey>> =
+        routes.filter { sufferScoreFor(it) <= 10 }
 
     private fun sufferScore(
         longestJourneys: List<Journey>,
         start: Location
-    ): Int {
-        return SOME_COMPLICATED_RESULT()
-    } 
+    ): Int = SOME_COMPLICATED_RESULT()
 }
