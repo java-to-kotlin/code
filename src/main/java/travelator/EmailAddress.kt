11aa6ada2eddb06f1a6ba8c262dd486242b6f52f
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-16 15:36:50 +0000

    single-expressions.22 : Fix splitAround

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -10,18 +10,17 @@ data class EmailAddress(
     companion object {
         @JvmStatic
         fun parse(value: String): EmailAddress =
-            value.split('@').let { (leftPart, rightPart) ->
+            value.splitAroundLast('@').let { (leftPart, rightPart) ->
                 EmailAddress(leftPart, rightPart)
             }
-
-        private fun String.split(divider: Char): Pair<String, String> {
-            val atIndex = lastIndexOf(divider)
-            require(!(atIndex < 1 || atIndex == length - 1)) {
-                "EmailAddress must be two parts separated by @"
-            }
-            val leftPart = substring(0, atIndex)
-            val rightPart = substring(atIndex + 1)
-            return Pair(leftPart, rightPart)
-        }
     }
 }
+
+private fun String.splitAroundLast(divider: Char): Pair<String, String> =
+    lastIndexOf(divider).let { index ->
+        require(index >= 1 && index != length - 1) {
+            "string must be two non-empty parts separated by $divider"
+        }
+        substring(0, index) to substring(index + 1)
+    }
+
