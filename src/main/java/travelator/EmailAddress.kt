c1424dcccc359df0eaaa9e101b810b40e7a316df
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-16 10:39:04 +0000

    single-expressions.2 : inline atIndex

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -10,13 +10,14 @@ data class EmailAddress(
     companion object {
         @JvmStatic
         fun parse(value: String): EmailAddress {
-            val atIndex = value.lastIndexOf('@') // <1>
-            require(!(atIndex < 1 || atIndex == value.length - 1)) { // <2>
+            require(!(
+                value.lastIndexOf('@') < 1 ||
+                    value.lastIndexOf('@') == value.length - 1)) {
                 "EmailAddress must be two parts separated by @"
             }
-            return EmailAddress( // <3>
-                value.substring(0, atIndex),
-                value.substring(atIndex + 1)
+            return EmailAddress(
+                value.substring(0, value.lastIndexOf('@')),
+                value.substring(value.lastIndexOf('@') + 1)
             )
         }
     }
