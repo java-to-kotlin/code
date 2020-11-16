353c164aa5f0d066983f27fbbb219a91f68829fe
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-16 14:16:16 +0000

    single-expressions.17 : Toss it

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -9,15 +9,15 @@ data class EmailAddress(
 
     companion object {
         @JvmStatic
-        fun parse(value: String): EmailAddress =
-            value.lastIndexOf('@').let { atIndex ->
-                require(!(atIndex < 1 || atIndex == value.length - 1)) {
-                    "EmailAddress must be two parts separated by @"
-                }
-                EmailAddress(
-                    value.substring(0, atIndex),
-                    value.substring(atIndex + 1)
-                )
+        fun parse(value: String): EmailAddress {
+            val atIndex = value.lastIndexOf('@')
+            require(!(atIndex < 1 || atIndex == value.length - 1)) {
+                "EmailAddress must be two parts separated by @"
             }
+            return EmailAddress(
+                value.substring(0, atIndex),
+                value.substring(atIndex + 1)
+            )
+        }
     }
 }
