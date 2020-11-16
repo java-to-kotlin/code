7549871e721f41e47892b7e22d8669f26986afad
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-16 14:30:07 +0000

    single-expressions.19 : Extract split

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -10,16 +10,21 @@ data class EmailAddress(
     companion object {
         @JvmStatic
         fun parse(value: String): EmailAddress {
+            val (leftPart, rightPart) = split(value)
+            return EmailAddress(
+                leftPart,
+                rightPart
+            )
+        }
+
+        private fun split(value: String): Pair<String, String> {
             val atIndex = value.lastIndexOf('@')
             require(!(atIndex < 1 || atIndex == value.length - 1)) {
                 "EmailAddress must be two parts separated by @"
             }
             val leftPart = value.substring(0, atIndex)
             val rightPart = value.substring(atIndex + 1)
-            return EmailAddress(
-                leftPart,
-                rightPart
-            )
+            return Pair(leftPart, rightPart)
         }
     }
 }
