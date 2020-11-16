14be1250e294fa455c8b24a1e89985fbeb05c9bc
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-16 12:04:10 +0000

    single-expressions.10 : When instead of if

--- src/main/java/travelator/EmailAddress.kt
+++ src/main/java/travelator/EmailAddress.kt
@@ -9,17 +9,16 @@ data class EmailAddress(
 
     companion object {
         @JvmStatic
-        fun parse(value: String): EmailAddress {
-            return emailAddress(value, value.lastIndexOf('@'))
-        }
+        fun parse(value: String) =
+            emailAddress(value, value.lastIndexOf('@'))
 
         private fun emailAddress(value: String, atIndex: Int): EmailAddress =
-            if ((atIndex < 1 || atIndex == value.length - 1)) {
-                throw IllegalArgumentException(
-                    "EmailAddress must be two parts separated by @"
-                )
-            } else {
-                EmailAddress(
+            when {
+                atIndex < 1 || atIndex == value.length - 1 ->
+                    throw IllegalArgumentException(
+                        "EmailAddress must be two parts separated by @"
+                    )
+                else -> EmailAddress(
                     value.substring(0, atIndex),
                     value.substring(atIndex + 1)
                 )
