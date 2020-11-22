97a5dbc1e9c3d4cb65136945b32ebdc4bd04cf73
Nat Pryce <sw@natpryce.com>
2020-11-22 22:45:42 +0000

    errors.5 : add addToo

--- src/main/java/travelator/Customers.kt
+++ src/main/java/travelator/Customers.kt
@@ -1,11 +1,22 @@
 package travelator
 
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Success
 import java.util.*
 
 interface Customers {
 
-    @Throws(DuplicateException::class) // <1>
+    @Throws(DuplicateException::class)
     fun add(name: String, email: String): Customer
+
+    fun addToo(name:String, email:String)
+        : Result<Customer, DuplicateException> =
+        try {
+            Success(add(name, email))
+        } catch (x: DuplicateException) {
+            Failure(x)
+        }
 
     fun find(id: String): Optional<Customer>
 }
