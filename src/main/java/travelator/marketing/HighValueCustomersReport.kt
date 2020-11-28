74b5ef84b0342e5fb929cfbfdf0edcf7191fd373
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-28 18:31:57 +0000

    extensions.4 : Fix compile

--- src/main/java/travelator/marketing/HighValueCustomersReport.kt
+++ src/main/java/travelator/marketing/HighValueCustomersReport.kt
@@ -34,11 +34,12 @@ object HighValueCustomersReport {
     @JvmStatic
     fun customerDataFrom(line: String): CustomerData {
         val parts = line.split("\t".toRegex()).toTypedArray()
-        val spend: Double = if (parts.size == 4) 0 else parts[4].toDouble() // <1>
+        val spend: Double = if (parts.size == 4) 0.0 else parts[4].toDouble()
         return CustomerData(
             parts[0],
             parts[1],
-            parts[2], parts[3].toInt(), // <2>
+            parts[2],
+            parts[3].toInt(),
             spend
         )
     }
