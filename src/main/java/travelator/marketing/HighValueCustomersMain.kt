e2beb0b9207fa42a3e49f99b2d80fbc121f6a88c
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-14 18:10:26 +0000

    exceptions-to-values.8 : Revert last, introduce using, exit properly

--- src/main/java/travelator/marketing/HighValueCustomersMain.kt
+++ src/main/java/travelator/marketing/HighValueCustomersMain.kt
@@ -1,31 +1,53 @@
 package travelator.marketing
 
+import java.io.Closeable
+import java.io.OutputStreamWriter
 import java.io.Reader
 import java.io.Writer
 import kotlin.system.exitProcess
 
 fun main() {
-    System.`in`.reader().use { reader ->
-        System.out.writer().use { writer ->
-            val errorLines = mutableListOf<ParseFailure>()
-            val reportLines = reader
-                .asLineSequence()
-                .toHighValueCustomerReport {
-                    errorLines += it
-                }
-            if (errorLines.isNotEmpty()) {
-                System.err.writer().use { error ->
-                    error.appendLine("Lines with errors")
-                    errorLines.asSequence().map { parseFailure ->
-                        "${parseFailure::class.simpleName} in ${parseFailure.line}"
-                    }.writeTo(error)
-                }
-                exitProcess(-1)
-            } else {
-                reportLines.writeTo(writer)
+    val statusCode = using(
+        System.`in`.reader(),
+        System.out.writer(),
+        System.err.writer()
+    ) { reader, writer, error ->
+        val errorLines = mutableListOf<ParseFailure>()
+        val reportLines = reader
+            .asLineSequence()
+            .toHighValueCustomerReport {
+                errorLines += it
+            }
+        if (errorLines.isEmpty()) {
+            reportLines.writeTo(writer)
+            0
+        } else {
+            errorLines.writeTo(error)
+            -1
+        }
+    }
+    exitProcess(statusCode)
+}
+
+inline fun <A : Closeable, B : Closeable, C : Closeable, R> using(
+    a: A,
+    b: B,
+    c: C,
+    block: (A, B, C) -> R
+): R =
+    a.use {
+        b.use {
+            c.use {
+                block(a, b, c)
             }
         }
     }
+
+private fun List<ParseFailure>.writeTo(error: OutputStreamWriter) {
+    error.appendLine("Lines with errors")
+    asSequence().map { parseFailure ->
+        "${parseFailure::class.simpleName} in ${parseFailure.line}"
+    }.writeTo(error)
 }
 
 fun Reader.asLineSequence() = buffered().lineSequence()
--- src/main/java/travelator/marketing/HighValueCustomersReport.kt
+++ src/main/java/travelator/marketing/HighValueCustomersReport.kt
@@ -1,6 +1,9 @@
 package travelator.marketing
 
-import com.natpryce.*
+import com.natpryce.Failure
+import com.natpryce.Result
+import com.natpryce.Success
+import com.natpryce.recover
 
 fun Sequence<String>.toHighValueCustomerReport(
     onErrorLine: (ParseFailure) -> Unit = {}
@@ -31,29 +34,21 @@ private fun Sequence<String>.withoutHeader() = drop(1)
 
 internal fun String.toCustomerData(): Result<CustomerData, ParseFailure> =
     split("\t").let { parts ->
-        parts
-            .takeUnless { it.size < 4 }
-            .asResultOr { NotEnoughFieldsFailure(this) }
-            .flatMap { parts ->
-                parts[3].toIntOrNull()
-                    .asResultOr { ScoreIsNotAnIntFailure(this) }
-                    .flatMap { score: Int ->
-                        (if (parts.size == 4) 0.0
-                        else parts[4].toDoubleOrNull())
-                            .asResultOr { SpendIsNotADoubleFailure(this) }
-                            .flatMap { spend ->
-                                Success(
-                                    CustomerData(
-                                        id = parts[0],
-                                        givenName = parts[1],
-                                        familyName = parts[2],
-                                        score = score,
-                                        spend = spend
-                                    )
-                                )
-                            }
-                    }
-            }
+        if (parts.size < 4)
+            return Failure(NotEnoughFieldsFailure(this))
+        val score = parts[3].toIntOrNull() ?:
+            return Failure(ScoreIsNotAnIntFailure(this))
+        val spend = if (parts.size == 4) 0.0 else parts[4].toDoubleOrNull() ?:
+            return Failure(SpendIsNotADoubleFailure(this))
+        Success(
+            CustomerData(
+                id = parts[0],
+                givenName = parts[1],
+                familyName = parts[2],
+                score = score,
+                spend = spend
+            )
+        )
     }
 
 sealed class ParseFailure(open val line: String)
