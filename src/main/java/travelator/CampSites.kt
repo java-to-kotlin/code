4801f75d3ae90cffc581627a7987d039127d0322
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-15 11:22:32 +0000

    methods-to-properties.6 : convert CampSites to Kotlin

--- src/main/java/travelator/CampSites.kt
+++ src/main/java/travelator/CampSites.kt
@@ -1,15 +1,10 @@
 package travelator
 
-import java.util.stream.Collectors
-
-fun Set<CampSite>.sitesInRegion(
+fun Iterable<CampSite>.sitesInRegion(
     countryISO: String,
     region: String
-): Set<CampSite> {
-    return stream()
-        .filter { campSite: CampSite ->
-            campSite.countryCode == countryISO &&
-                campSite.region.equals(region, ignoreCase = true)
-        }
-        .collect(Collectors.toUnmodifiableSet())
-}
+): Set<CampSite> =
+    filter { site ->
+        site.countryCode == countryISO &&
+            site.region.equals(region, ignoreCase = true)
+    }.toSet()
