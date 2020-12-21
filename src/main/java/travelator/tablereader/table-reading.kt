294ca144f27f8be1fd0f4c99d776e8530f10229c
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-21 20:57:46 +0000

    table-reader.11 : zip sides of map

--- src/main/java/travelator/tablereader/table-reading.kt
+++ src/main/java/travelator/tablereader/table-reading.kt
@@ -4,4 +4,8 @@ fun readTable(lines: List<String>): List<Map<String, String>> {
     return lines.map(::parseLine)
 }
 
-private fun parseLine(line: String) = mapOf("0" to "field0", "1" to "field1")
+private fun parseLine(line: String): Map<String, String> {
+    val keys = listOf("0", "1")
+    val values = listOf("field0", "field1")
+    return keys.zip(values).toMap()
+}
