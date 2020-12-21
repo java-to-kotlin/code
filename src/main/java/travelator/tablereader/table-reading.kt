9aac3877bb5596ff54090ef4d063308791065d2a
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-21 21:03:27 +0000

    table-reader.13 : parse the values

--- src/main/java/travelator/tablereader/table-reading.kt
+++ src/main/java/travelator/tablereader/table-reading.kt
@@ -5,7 +5,7 @@ fun readTable(lines: List<String>): List<Map<String, String>> {
 }
 
 private fun parseLine(line: String): Map<String, String> {
-    val values = listOf("field0", "field1")
+    val values = line.split(",")
     val keys = values.indices.map(Int::toString)
     return keys.zip(values).toMap()
 }
