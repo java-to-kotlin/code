55d4cb7531f8e492b8509302b9798a96d9c33b6b
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-22 22:45:45 +0000

    table-reader.52 : Organise tests

--- src/test/java/travelator/tablereader/TableReaderTests.kt
+++ src/test/java/travelator/tablereader/TableReaderTests.kt
@@ -6,72 +6,43 @@ import java.io.StringReader
 
 class TableReaderTests {
     @Test
-    fun `empty list returns empty list`() {
-        assertEquals(
-            emptyList<Map<String, String>>(),
-            readTable(emptyList())
+    fun `empty input returns empty`() {
+        checkReadTable(
+            lines = emptyList(),
+            shouldReturn = emptyList()
         )
     }
 
     @Test
     fun `one line of input with default field names`() {
-        assertEquals(
-            listOf(
+        checkReadTable(
+            lines = listOf("field0,field1"),
+            shouldReturn = listOf(
                 mapOf("0" to "field0", "1" to "field1")
-            ),
-            readTable(listOf(
-                "field0,field1"
-            ))
-        )
-    }
-
-    @Test
-    fun `empty line returns empty map`() {
-        assertEquals(
-            listOf(
-                emptyMap<String, String>()
-            ),
-            readTable(listOf(
-                ""
-            ))
-        )
-    }
-
-    @Test
-    fun `two lines of input with default field names`() {
-        assertEquals(
-            listOf(
-                mapOf("0" to "row0field0", "1" to "row0field1"),
-                mapOf("0" to "row1field0", "1" to "row1field1")
-            ),
-            readTable(listOf(
-                "row0field0,row0field1",
-                "row1field0,row1field1"
-            ))
-        )
-    }
-
-    @Test
-    fun `takes headers from header line`() {
-        assertEquals(
-            listOf(
-                mapOf("H0" to "field0", "H1" to "field1")
-            ),
-            readTableWithHeader(
-                listOf(
-                    "H0,H1",
-                    "field0,field1"
-                )
             )
         )
     }
 
     @Test
-    fun `empty list if header line but no body`() {
-        assertEquals(
-            emptyList<String>(),
-            readTableWithHeader(
-                listOf("H0,H1")
+    fun `empty line returns empty map`() {
+        checkReadTable(
+            lines = listOf(""),
+            shouldReturn = listOf(
+                emptyMap()
+            )
+        )
+    }
+
+    @Test
+    fun `two lines of input with default field names`() {
+        checkReadTable(
+            lines = listOf(
+                "row0field0,row0field1",
+                "row1field0,row1field1"
+            ),
+            shouldReturn = listOf(
+                mapOf("0" to "row0field0", "1" to "row0field1"),
+                mapOf("0" to "row1field0", "1" to "row1field1")
             )
         )
     }
@@ -79,36 +50,61 @@ class TableReaderTests {
     @Test
     fun `can specify header names when there is no header row`() {
         val headers = listOf("apple", "banana")
-        assertEquals(
-            listOf(
+        checkReadTable(
+            lines = listOf("field0,field1"),
+            withHeaderProvider = headers::get,
+            shouldReturn = listOf(
                 mapOf(
                     "apple" to "field0",
                     "banana" to "field1",
                 )
-            ),
-            readTable(
-                listOf("field0,field1"),
-                headers::get
             )
         )
     }
 
     @Test
+    fun `readTableWithHeader takes headers from header line`() {
+        checkReadTableWithHeader(
+            lines = listOf(
+                "H0,H1",
+                "field0,field1"
+            ),
+            shouldReturn = listOf(
+                mapOf("H0" to "field0", "H1" to "field1")
+            )
+        )
+    }
+
+    @Test
+    fun `readTableWithHeader empty if lines empty`() {
+        checkReadTableWithHeader(
+            lines = emptyList(),
+            shouldReturn = emptyList(),
+        )
+    }
+
+    @Test
+    fun `readTableWithHeader empty if header line but no body`() {
+        checkReadTableWithHeader(
+            lines = listOf("H0,H1"),
+            shouldReturn = emptyList(),
+        )
+    }
+
+    @Test
     fun `can specify splitter`() {
-        assertEquals(
-            listOf(
+        checkReadTableWithHeader(
+            lines = listOf(
+                "header1\theader2",
+                "field0\tfield1"
+            ),
+            withSplitter = splitOnTab,
+            shouldReturn = listOf(
                 mapOf(
                     "header1" to "field0",
                     "header2" to "field1",
                 )
             ),
-            readTableWithHeader(
-                listOf(
-                    "header1\theader2",
-                    "field0\tfield1"
-                ),
-                splitOnTab
-            )
         )
     }
 
@@ -130,34 +126,33 @@ class TableReaderTests {
             )
         }
     }
-
-    @Test
-    fun `readTableWithHeader on empty list returns empty list`() {
-        assertEquals(
-            emptyList<String>(),
-            readTableWithHeader(
-                emptyList()
-            )
-        )
-    }
 }
 
-private fun readTableWithHeader(
+private fun checkReadTable(
     lines: List<String>,
-    splitter: (String) -> List<String> = splitOnComma
-): List<Map<String, String>> =
-    readTableWithHeader(
-        lines.asSequence(),
-        splitter
-    ).toList()
+    withHeaderProvider: (Int) -> String = Int::toString,
+    shouldReturn: List<Map<String, String>>,
+) {
+    assertEquals(
+        shouldReturn,
+        readTable(
+            lines.asSequence().constrainOnce(),
+            headerProvider = withHeaderProvider,
+            splitter = splitOnComma
+        ).toList()
+    )
+}
 
-private fun readTable(
+private fun checkReadTableWithHeader(
     lines: List<String>,
-    headerProvider: (Int) -> String = Integer::toString,
-    splitter: (String) -> List<String> = splitOnComma
-): List<Map<String, String>> =
-    readTable(
-        lines.asSequence(),
-        headerProvider,
-        splitter
-    ).toList()
+    withSplitter: (String) -> List<String> = splitOnComma,
+    shouldReturn: List<Map<String, String>>,
+) {
+    assertEquals(
+        shouldReturn,
+        readTableWithHeader(
+            lines.asSequence().constrainOnce(),
+            splitter = withSplitter
+        ).toList()
+    )
+}
