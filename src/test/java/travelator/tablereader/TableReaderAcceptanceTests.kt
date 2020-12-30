9f59b5c7df57ffa9edc1f108dd20179efd5cd824
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-12-30 17:57:39 +0000

    table-reader.2 : sketch usage (fails tests)

--- src/test/java/travelator/tablereader/TableReaderAcceptanceTests.kt
+++ src/test/java/travelator/tablereader/TableReaderAcceptanceTests.kt
@@ -1,9 +1,35 @@
 package travelator.tablereader
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TableReaderAcceptanceTests {
+    data class Measurement(
+        val t: Double,
+        val x: Double,
+        val y: Double,
+    )
+
     @Test
-    fun test() {
+    fun `acceptance test`() {
+        val input = listOf(
+            "time,x,y",
+            "0.0,  1,  1",
+            "0.1,1.1,1.2",
+            "0.2,1.2,1.4",
+        )
+        val expected = listOf(
+            Measurement(0.0, 1.0, 1.0),
+            Measurement(0.1, 1.1, 1.2),
+            Measurement(0.2, 1.2, 1.4)
+        )
+        assertEquals(
+            expected,
+            someFunction(input)
+        )
+    }
+
+    private fun someFunction(input: List<String>): List<Measurement> {
+        TODO("Not yet implemented")
     }
 }
