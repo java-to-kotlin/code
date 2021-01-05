85584f983b73702299bed606c5b53f4a552bb25c
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-05 22:34:02 +0000

    streams-to-sequences.2 : tidy

--- src/main/java/travelator/analytics/MarketingAnalytics.kt
+++ src/main/java/travelator/analytics/MarketingAnalytics.kt
@@ -1,7 +1,6 @@
 package travelator.analytics
 
-import java.util.function.Function
-import java.util.stream.Collectors
+import java.util.stream.Collectors.groupingBy
 
 class MarketingAnalytics(
     private val eventStore: EventStore
@@ -11,21 +10,17 @@ class MarketingAnalytics(
     ): Double {
         val eventsForSuccessfulBookings = eventStore
             .queryAsStream("type=CompletedBooking&timerange=$timeRange")
-            .flatMap { event: Map<String?, Any?> ->
-                val interactionId = event["interactionId"] as String?
+            .flatMap { event ->
+                val interactionId = event["interactionId"] as String
                 eventStore.queryAsStream("interactionId=$interactionId")
             }
         val bookingEventsByInteractionId = eventsForSuccessfulBookings.collect(
-            Collectors.groupingBy(
-                Function { event: Map<String, Any> ->
-                    event["interactionId"] as String?
-                }
-            )
+            groupingBy { event -> event["interactionId"] as String }
         )
         val averageNumberOfEventsPerCompletedBooking = bookingEventsByInteractionId
             .values
             .stream()
-            .mapToInt { obj: List<Map<String, Any>> -> obj.size }
+            .mapToInt { it.size }
             .average()
         return averageNumberOfEventsPerCompletedBooking.orElse(Double.NaN)
     }
