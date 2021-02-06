969152046fd78aca118be80f0726368a3cb92b7d
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-06 20:23:33 +0000

    nullability.9 : Remove findLongestLegOver

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -1,17 +1,8 @@
 package travelator
 
 import java.time.Duration
-import java.util.*
 
 object Legs {
-    @JvmStatic
-    fun findLongestLegOver(
-        legs: List<Leg>,
-        duration: Duration
-    ): Optional<Leg> {
-        return Optional.ofNullable(longestLegOver(legs, duration))
-    }
-
     fun longestLegOver(
         legs: List<Leg>,
         duration: Duration
