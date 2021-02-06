c6c3ae15b76234abacdf25cfcab6e306c12d49b5
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-06 18:59:51 +0000

    nullability.5 : Extract function longestLegOver

--- src/main/java/travelator/Legs.kt
+++ src/main/java/travelator/Legs.kt
@@ -6,17 +6,22 @@ import java.util.*
 object Legs {
     @JvmStatic
     fun findLongestLegOver(
-        legs: List<Leg>,
+        legs: List<Leg>, 
         duration: Duration
     ): Optional<Leg> {
+        var result: Leg? = longestLegOver(legs, duration)
+        return Optional.ofNullable(result)
+    }
+
+    fun longestLegOver(legs: List<Leg>, duration: Duration): Leg? {
         var result: Leg? = null
         for (leg in legs) {
             if (isLongerThan(leg, duration))
-                if (result == null ||
+                if (result == null || 
                     isLongerThan(leg, result.plannedDuration))
                     result = leg
         }
-        return Optional.ofNullable(result)
+        return result
     }
 
     private fun isLongerThan(leg: Leg, duration: Duration): Boolean {
