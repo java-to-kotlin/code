56e1d252e1afe8c799b6bfd3f7872b50ad8b2650
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-15 21:17:53 +0000

    accumulator.14 : Remove mutableMap and unused

--- src/main/java/travelator/itinerary/CostSummaryCalculator.kt
+++ src/main/java/travelator/itinerary/CostSummaryCalculator.kt
@@ -1,46 +1,25 @@
 package travelator.itinerary
 
+import travelator.money.CurrencyConversion
 import travelator.money.ExchangeRates
 import travelator.money.Money
-import java.util.Currency
+import java.util.*
 
 class CostSummaryCalculator(
     private val userCurrency: Currency,
     private val exchangeRates: ExchangeRates
 ) {
-    private val currencyTotals = mutableMapOf<Currency, Money>()
-
     fun summarise(costs: Iterable<Money>): CostSummary {
-        val currencyTotals = mutableMapOf<Currency, Money>()
-        costs.forEach {
-            currencyTotals.merge(it.currency, it, Money::plus)
-        }
-        val lines = currencyTotals.values
+        val currencyTotals: List<Money> = costs
+            .groupBy { it.currency }
+            .values
+            .map { moneys -> moneys.reduce(Money::add) }
+        val lines: List<CurrencyConversion> = currencyTotals
             .sortedBy { it.currency.currencyCode }
             .map { exchangeRates.convert(it, userCurrency) }
         val total = lines
             .map { it.toMoney }
             .fold(Money(0, userCurrency), Money::add)
         return CostSummary(lines, total)
-    }
-
-    fun addCost(cost: Money) {
-        currencyTotals.merge(cost.currency, cost, Money::add)
-    }
-
-    fun summarise(): CostSummary {
-        val lines = currencyTotals.values
-            .sortedBy { it.currency.currencyCode }
-            .map { exchangeRates.convert(it, userCurrency) }
-
-        val total = lines
-            .map { it.toMoney }
-            .fold(Money.of(0, userCurrency), Money::add)
-
-        return CostSummary(lines, total)
-    }
-
-    fun reset() {
-        currencyTotals.clear()
     }
 }
