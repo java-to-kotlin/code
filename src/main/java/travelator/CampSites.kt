571faa0f1c697d53776696245af72687c892c397
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-15 11:45:51 +0000

    methods-to-properties.5 : convert Campsites to Kotlin pass 2

--- src/main/java/travelator/CampSites.kt
+++ src/main/java/travelator/CampSites.kt
@@ -2,17 +2,14 @@ package travelator
 
 import java.util.stream.Collectors
 
-object CampSites {
-    fun sitesInRegion(
-        sites: Set<CampSite>,
-        countryISO: String,
-        region: String?
-    ): Set<CampSite> {
-        return sites.stream()
-            .filter { campSite: CampSite ->
-                campSite.countryCode == countryISO &&
-                    campSite.region.equals(region, ignoreCase = true) // <1>
-            }
-            .collect(Collectors.toUnmodifiableSet())
-    }
+fun Set<CampSite>.sitesInRegion(
+    countryISO: String,
+    region: String
+): Set<CampSite> {
+    return stream()
+        .filter { campSite: CampSite ->
+            campSite.countryCode == countryISO &&
+                campSite.region.equals(region, ignoreCase = true)
+        }
+        .collect(Collectors.toUnmodifiableSet())
 }
