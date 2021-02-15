6a425eaa20fb6f3bf618f6157ce827c42df839e2
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-15 18:08:52 +0000

    accumulator.6 : Move some mutation from CostSummary

--- src/main/java/travelator/itinerary/CostSummary.kt
+++ src/main/java/travelator/itinerary/CostSummary.kt
@@ -4,24 +4,17 @@ import travelator.money.CurrencyConversion
 import travelator.money.Money
 import java.util.Currency
 
-class CostSummary(userCurrency: Currency) {
-    private val _lines = mutableListOf<CurrencyConversion>()
+class CostSummary(
+    userCurrency: Currency,
+    val lines: List<CurrencyConversion>
+) {
 
     var total: Money = Money.of(0, userCurrency)
         private set
 
-    val lines: List<CurrencyConversion>
-        get() = _lines.toList()
-
-    constructor(
-        userCurrency: Currency,
-        lines: List<CurrencyConversion>
-    ): this(userCurrency) {
-        lines.forEach(::addLine)
-    }
-
-    fun addLine(line: CurrencyConversion) {
-        _lines.add(line)
-        total += line.toMoney
+    init {
+        lines.forEach {
+            total += it.toMoney
+        }
     }
 }
