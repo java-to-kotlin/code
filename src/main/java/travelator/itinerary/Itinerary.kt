32c920cbe525a639cdfbb24ee989c4c1642ab755
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-02-20 22:05:47 +0000

    encapsulated-collections.11 : Itinerary baseline

--- src/main/java/travelator/itinerary/Itinerary.kt
+++ src/main/java/travelator/itinerary/Itinerary.kt
@@ -1,9 +1,14 @@
 package travelator.itinerary
 
 import travelator.Id
+import java.time.Duration
 
 class Itinerary(
     val id: Id<Itinerary>,
     val route: Route
 ) {
+
+    fun hasJourneyLongerThan(duration: Duration) =
+        route.any { it.duration > duration }
+
 }
