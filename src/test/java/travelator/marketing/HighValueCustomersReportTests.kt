b93055f3f9206fd39ecd65eee1f7904ae5f0b122
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-03-04 10:29:41 +0000

    io-to-data.1 : convert tests to Kotlin

--- src/test/java/travelator/marketing/HighValueCustomersReportTests.kt
+++ src/test/java/travelator/marketing/HighValueCustomersReportTests.kt
@@ -1,63 +1,62 @@
-package travelator.marketing;
+package travelator.marketing
 
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.StringReader
+import java.io.StringWriter
 
 class HighValueCustomersReportTests {
 
     @Test
-    public void test() throws IOException {
-        List<String> input = List.of(
-            "ID\tFirstName\tLastName\tScore\tSpend",
-            "1\tFred\tFlintstone\t11\t1000.00",
-            "4\tBetty\tRubble\t10\t2000.00",
-            "2\tBarney\tRubble\t0\t20.00",
-            "3\tWilma\tFlintstone\t9\t0.00"
-        );
-        List<String> expected = List.of(
-            "ID\tName\tSpend",
-            "4\tRUBBLE, Betty\t2000.00",
-            "1\tFLINTSTONE, Fred\t1000.00",
-            "\tTOTAL\t3000.00"
-        );
-        check(input, expected);
+    fun test() {
+        check(
+            inputLines = listOf(
+                "ID\tFirstName\tLastName\tScore\tSpend",
+                "1\tFred\tFlintstone\t11\t1000.00",
+                "4\tBetty\tRubble\t10\t2000.00",
+                "2\tBarney\tRubble\t0\t20.00",
+                "3\tWilma\tFlintstone\t9\t0.00"
+            ),
+            expectedLines = listOf(
+                "ID\tName\tSpend",
+                "4\tRUBBLE, Betty\t2000.00",
+                "1\tFLINTSTONE, Fred\t1000.00",
+                "\tTOTAL\t3000.00"
+            )
+        )
+    }
+
+
+    @Test
+    fun emptyTest() {
+        check(
+            inputLines = listOf(
+                "ID\tFirstName\tLastName\tScore\tSpend"
+            ),
+            expectedLines = listOf(
+                "ID\tName\tSpend",
+                "\tTOTAL\t0.00"
+            )
+        )
     }
 
     @Test
-    public void emptyTest() throws IOException {
-        List<String> input = List.of(
-            "ID\tFirstName\tLastName\tScore\tSpend"
-        );
-        List<String> expected = List.of(
-            "ID\tName\tSpend",
-            "\tTOTAL\t0.00"
-        );
-        check(input, expected);
-    }
-
-    @Test
-    public void emptySpendIs0() {
+    fun emptySpendIs0() {
         assertEquals(
-            new CustomerData("1", "Fred", "Flintstone", 0, 0D),
-            HighValueCustomersReportKt.toCustomerData("1\tFred\tFlintstone\t0")
-        );
+            CustomerData("1", "Fred", "Flintstone", 0, 0.0),
+            "1\tFred\tFlintstone\t0".toCustomerData()
+        )
     }
 
-    private void check(
-        List<String> inputLines,
-        List<String> expectedLines
-    ) throws IOException {
-        var output = new StringWriter();
-        HighValueCustomersReportKt.generate(
-            new StringReader(String.join("\n", inputLines)),
+    private fun check(
+        inputLines: List<String>,
+        expectedLines: List<String>
+    ) {
+        val output = StringWriter()
+        generate(
+            StringReader(inputLines.joinToString("\n")),
             output
-        );
-        assertEquals(String.join("\n", expectedLines), output.toString());
+        )
+        assertEquals(expectedLines.joinToString("\n"), output.toString())
     }
 }
