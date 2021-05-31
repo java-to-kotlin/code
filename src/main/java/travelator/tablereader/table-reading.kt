1dddcf2c3a4d14f3a4a68f2571a6cd9362a11eef
Duncan McGregor <duncan@oneeyedmen.com>
2021-05-31 17:02:46 +0100

    table-reader.9 : introduce map

--- src/main/java/travelator/tablereader/table-reading.kt
+++ src/main/java/travelator/tablereader/table-reading.kt
@@ -1,9 +1,7 @@
 package travelator.tablereader
 
 fun readTable(lines: List<String>): List<Map<String, String>> {
-    return if (lines.isEmpty())
-        emptyList()
-    else listOf(
+    return lines.map {
         mapOf("0" to "field0", "1" to "field1")
-    )
+    }
 }
